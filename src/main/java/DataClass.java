973f3edc6c9bf25fddd629bec27fc35fc979537e
num13 <num13-ne@mail.ru>
2024-06-28 23:11:31 +0300

    Change in test

--- src/main/java/DataClass.java
+++ src/main/java/DataClass.java
@@ -1,44 +1,46 @@
 import com.github.javafaker.Faker;
 import lombok.Value;
 
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Random;
 
 public class DataClass {
-    private DataClass(){
+    private DataClass() {
     }
 
-    public static String genDate(int addedDays){
+    public static String genDate(int addedDays) {
         return LocalDate.now().plusDays(addedDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    public static String genCity(){
-        var cities = new String[]{"Москва","Тверь","Санкт-Петербург","Тула","Казань"};
+    public static String genCity() {
+        var cities = new String[]{"Москва", "Тверь", "Санкт-Петербург", "Тула", "Казань"};
         return cities[new Random().nextInt(cities.length)];
     }
 
-    public static String genName (String loc){
+    public static String genName(String loc) {
         var faker = new Faker(new Locale(loc));
         return faker.name().lastName() + " " + faker.name().firstName();
     }
 
-    public static String genPhone (String loc){
+    public static String genPhone(String loc) {
         var faker = new Faker(new Locale(loc));
         return faker.phoneNumber().phoneNumber();
     }
 
-    public static class Registration{
-        private Registration(){
+    public static class Registration {
+        private Registration() {
         }
 
-        public static UserInfo generateUser(String loc){
-           return new UserInfo(genCity(), genName(loc) ,genPhone(loc));
+        public static UserInfo generateUser(String loc) {
+            return new UserInfo(genCity(), genName(loc), genPhone(loc));
         }
     }
+
     @Value
-    public static class UserInfo{
+    public static class UserInfo {
         String city;
         String name;
         String phone;
--- src/test/java/PatternsTest.java
+++ src/test/java/PatternsTest.java
@@ -6,26 +6,26 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PatternsTest {
     @BeforeEach
-    void starting(){
+    void starting() {
         open("http://localhost:9999");
     }
+
     @Test
-    void successPlan(){
+    void successPlan() {
         var user = DataClass.Registration.generateUser("ru");
         int addedDays1 = 4;
         String firstDate = DataClass.genDate(addedDays1);
         int addedDays2 = 7;
         String secondDate = DataClass.genDate(addedDays2);
         $("[data-test-id=city] input").setValue(user.getCity());
-        $("[data-test-id=date] input").sendKeys(Keys.SHIFT,Keys.HOME,Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
         $("[data-test-id=date] input").setValue(firstDate);
         $("[data-test-id=name] input").setValue(user.getName());
         $("[data-test-id=phone] input").setValue(user.getPhone());
@@ -35,11 +35,11 @@ public class PatternsTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldBe(exactText("Встреча успешно запланирована на " + firstDate))
                 .shouldBe(visible);
-        $("[data-test-id=date] input").sendKeys(Keys.SHIFT,Keys.HOME,Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
         $("[data-test-id=date] input").setValue(secondDate);
         $(byText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .notification__content")
-                .shouldBe(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
